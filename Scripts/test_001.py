0c19a0dc932e4c99a25d40544eaaf04cbfa84428
panshijian <panshijian>
2018-07-06 16:00:52 +0800

    init projiect

---
+++ Scripts/test_001.py
@@ -0,0 +1,9 @@
+import pytest
+
+class Test_001:
+    def test_01(self):
+        # print("test_01---->")
+        assert 0
+    def test_02(self):
+        # print("test_02----->")
+        assert 0
