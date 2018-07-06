07630f3e4657520fdb9c840745b4016281632a01
panshijian <panshijian>
2018-07-06 19:04:23 +0800

    app_项目1

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -6,7 +6,7 @@ class Test_001:
 
 
     def test_01(self):
-        @allure.attach("描述","我是测试步骤1")
+        allure.attach("描述","我是测试步骤1")
 
 
 
@@ -16,5 +16,5 @@ class Test_001:
     @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     def test_02(self):
         # print("test_02----->")
-        @allure.attach("描述","这是第二条测试")
+        allure.attach("描述","这是第二条测试")
         assert 1
